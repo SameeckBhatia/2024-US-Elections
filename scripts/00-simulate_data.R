9042401251260299696810839a17e95b65d7bf85
SameeckBhatia <canadianmg@outlook.com>
2024-10-12 21:16:42 -0400

    Simulating mock data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,52 +1,33 @@
 #### Preamble ####
-# Purpose: Simulates a dataset of Australian electoral divisions, including the 
-  #state and party that won each division.
-# Author: Rohan Alexander
-# Date: 26 September 2024
-# Contact: rohan.alexander@utoronto.ca
+# Purpose: Simulates a mock dataset of surveyed individuals
+# Author: Sameeck Bhatia
+# Date: 12 October 2024
+# Contact: sameeck.bhatia@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: The `tidyverse` package must be installed
-# Any other information needed? Make sure you are in the `starter_folder` rproj
+# Any other information needed? None
 
 
 #### Workspace setup ####
 library(tidyverse)
-set.seed(853)
+set.seed(12)
 
 
 #### Simulate data ####
-# State names
-states <- c(
-  "New South Wales",
-  "Victoria",
-  "Queensland",
-  "South Australia",
-  "Western Australia",
-  "Tasmania",
-  "Northern Territory",
-  "Australian Capital Territory"
-)
+states <- c("AL", "AK", "AR", "AZ", "CA", "CO", "CT")
+races <- c("w", "b", "h", "a", "n", "o")
+parties <- c("d", "r", "i")
+voter_types <- c("registered", "likely")
 
-# Political parties
-parties <- c("Labor", "Liberal", "Greens", "National", "Other")
-
-# Create a dataset by randomly assigning states and parties to divisions
-analysis_data <- tibble(
-  division = paste("Division", 1:151),  # Add "Division" to make it a character
-  state = sample(
-    states,
-    size = 151,
-    replace = TRUE,
-    prob = c(0.25, 0.25, 0.15, 0.1, 0.1, 0.1, 0.025, 0.025) # Rough state population distribution
-  ),
-  party = sample(
-    parties,
-    size = 151,
-    replace = TRUE,
-    prob = c(0.40, 0.40, 0.05, 0.1, 0.05) # Rough party distribution
-  )
+simulated_data <- data.frame(
+  "id" = sample(10000:99999, 10000, replace = FALSE),
+  "state" = sample(states, 10000, replace = TRUE),
+  "race" = sample(races, 10000, replace = TRUE, prob = c(60, 13, 19, 6, 1, 1)),
+  "party" = sample(parties, 10000, replace = TRUE, prob = c(51, 47, 2)),
+  "last_election" = sample(parties, 10000, replace = TRUE, prob = c(48, 46, 6)),
+  "voter_type" = sample(voter_types, 10000, replace = TRUE, prob = c(66, 34))
 )
 
 
 #### Save data ####
-write_csv(analysis_data, "data/00-simulated_data/simulated_data.csv")
+write_csv(simulated_data, "data/00-simulated_data/simulated_data.csv")
